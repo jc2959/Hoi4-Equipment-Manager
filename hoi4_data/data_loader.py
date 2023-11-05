d5967ba9dd77cf82c3986d90f5c126d620524d71
blueknightc <cottrelljjoshua@gmail.com>
2023-11-05 14:50:58 +0000

    Data Loader fixes
    Updated REGEX to improve data loading

--- hoi4_data/data_loader.py
+++ hoi4_data/data_loader.py
@@ -1,6 +1,7 @@
 import os
 from json.decoder import JSONDecodeError
 from re import Match, sub as re_sub
+import re
 import json
 from typing import Callable, Dict, Type
 
@@ -15,16 +16,10 @@ def convert_to_json(file_contents: str) -> Dict:
     :return: A Json Object that contains the same information as the input file's contents
     """
 
-    json_obj = {}
-
     # Applies regex to reformat the file's contents
     json_str = apply_json_regex(file_contents)
 
-    try:
-        json_obj = json.loads(json_str)
-    except JSONDecodeError:
-        print("COULD NOT LOAD FOLLOWING")
-        print(json_str)
+    json_obj = json.loads(json_str)
 
     return json_obj
 
@@ -38,36 +33,52 @@ def apply_json_regex(hoi4_formatted_str: str) -> str:
     """
     # Regex patterns
     comments_regex_pattern = "(#[^\n]*)\n"
-    attribute_regex_pattern = "((?:[^\"{}\\s=\\d])+|[^\\d\\s\\.][^\"{}\\s=]+)[\" \"\\n}]"
+    exist_quotemarks_regex_pattern = "\"([^\"]+)\""
+    attribute_regex_pattern = "([^\\d\\s\\.{}][^\"{}\\s=]+)[\\s]*[\" \"=\\n}]"
     newline_regex_pattern = "[^\\s][^{\\n]*(\\n)"
     lists_regex_pattern = "{[^{}=]+}"
     list_elements_regex_pattern = "\"[\\s]+\""
     lst_element_regex_pattern = ",\\s*[]}]"
     bad_commas_regex_pattern = "[{[][\\s]*,"
+    boolean_regex_pattern = "\\s((?:\"yes\")|(?:\"no\"))[\\s]*\\n"
+    trailing_zeros_regex_pattern = "\\s0([\\d]+)"
+    bad_characters_regex_pattern = "[^\\w\\s_={}\\.\"-()-]|Â"
 
     # Replacement functions
-    comments_rep_func: Callable[[Match], str] = lambda match: ""
+    remove_rep_func: Callable[[Match], str] = lambda match: ""
+    exist_quotemarks_rep_func: Callable[[Match], str] = lambda match: match.group(1).replace(" ", "¬")
+    just_group_rep_func: Callable[[Match], str] = lambda match: match.group(1)
     attr_rep_func: Callable[[Match], str] = lambda match: \
-        match.group().replace(match.group(1), "\"" + match.group(1) + "\"")
+        match.group().replace(match.group(1), "\"" + match.group(1).strip() + "\"")
     newline_rep_func: Callable[[Match], str] = lambda match: match.group().replace("\n", ",")
-    lists_rep_func: Callable[[Match], str] = lambda match: match.group().replace("{", "[").replace("}", "]")
+    lists_rep_func: Callable[[Match], str] = lambda match: match.group().replace("{", "[ ").replace("}", " ]")
     list_elements_rep_func: Callable[[Match], str] = lambda match: match.group().replace("\" ", "\",")
     lst_element_rep_func: Callable[[Match], str] = lambda match: match.group().replace(",", "")
+    boolean_rep_func: Callable[[Match], str] = lambda match: \
+        match.group().replace(match.group(1), "true" if match.group(1) == "\"yes\"" else "false")
 
     new_file_contents = hoi4_formatted_str
     new_file_contents = "{\n" + new_file_contents + "\n}"
 
+    new_file_contents = new_file_contents.replace(">", "=")
+    new_file_contents = new_file_contents.replace("<", "=")
+
     # Applies the replacement functions where the regex patterns are found
-    new_file_contents = re_sub(comments_regex_pattern, comments_rep_func, new_file_contents)
+    new_file_contents = re_sub(comments_regex_pattern, remove_rep_func, new_file_contents)
+    new_file_contents = re_sub(bad_characters_regex_pattern, remove_rep_func, new_file_contents)
+    new_file_contents = re_sub(exist_quotemarks_regex_pattern, exist_quotemarks_rep_func, new_file_contents)
     new_file_contents = re_sub(attribute_regex_pattern, attr_rep_func, new_file_contents)
+    new_file_contents = re_sub(boolean_regex_pattern, boolean_rep_func, new_file_contents)
     new_file_contents = re_sub(newline_regex_pattern, newline_rep_func, new_file_contents)
     new_file_contents = re_sub(lists_regex_pattern, lists_rep_func, new_file_contents)
     new_file_contents = re_sub(list_elements_regex_pattern, list_elements_rep_func, new_file_contents)
     new_file_contents = re_sub(lst_element_regex_pattern, lst_element_rep_func, new_file_contents)
     new_file_contents = re_sub(bad_commas_regex_pattern, lst_element_rep_func, new_file_contents)
+    new_file_contents = re_sub(trailing_zeros_regex_pattern, just_group_rep_func, new_file_contents)
 
     # Removes any bad double quotes
     new_file_contents = new_file_contents.replace("\"\"", "\"")
+    new_file_contents = new_file_contents.replace("¬", " ")
 
     new_file_contents = new_file_contents.replace("=", ":")
 
@@ -83,7 +94,7 @@ def load_all_data(path_to_hoi4_files: str, hoi4loadable_type: Type) -> Dict[str,
     :param hoi4loadable_type: The type of Hoi4 Loadables to load
     :return: A dictionary of the Hoi4 Loadables indexed by name
     """
-    types_parent_dir = hoi4loadable_type.__name__.lower().replace("-", "\\")
+    types_parent_dir = hoi4loadable_type.__name__.lower().replace("_", "\\")
 
     data_instance: Hoi4Loadable = hoi4loadable_type({})
     header = data_instance.header
@@ -95,7 +106,7 @@ def load_all_data(path_to_hoi4_files: str, hoi4loadable_type: Type) -> Dict[str,
     for roots, dirs, files in os.walk(path_to_hoi4_files):
         if roots.split("\\")[-1] == last_dir:
             if types_parent_dir in roots:
-                all_paths.extend([f"{roots}\\{name}" for name in files])
+                all_paths.extend([f"{roots}\\{name}" for name in files if ".txt" in name])
                 break
 
     return_data: Dict[str, Hoi4Loadable] = {}
@@ -105,7 +116,10 @@ def load_all_data(path_to_hoi4_files: str, hoi4loadable_type: Type) -> Dict[str,
 
         json_obj = convert_to_json(file_content)
 
-        if json_obj == {}:
+        if len(json_obj) == 0:
+            continue
+
+        if header not in json_obj.keys() or not isinstance(json_obj[header], dict):
             continue
 
         for key, value in json_obj[header].items():
